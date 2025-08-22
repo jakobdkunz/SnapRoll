df2c58fc2e9c966641b431d16f92491839d5f52c
Jack Kunz <jacquesvfd@gmail.com>
2025-08-21 21:04:13 -0400

    Fix Next.js configs for Vercel deployment

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -1,9 +1,5 @@
 const nextConfig = {
   transpilePackages: ['@snaproll/lib'],
   experimental: { typedRoutes: true },
-  server: {
-    hostname: '0.0.0.0',
-    port: 3002,
-  },
 };
 export default nextConfig;
