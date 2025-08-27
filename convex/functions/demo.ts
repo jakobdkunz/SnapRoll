8dadcd8a851513aaa4d0f7e5f8c82dd6689af36d
Jack Kunz <jacquesvfd@gmail.com>
2025-08-27 12:49:54 -0400

    Fix Convex TypeScript errors and reinitialize with new deployment

--- convex/functions/demo.ts
+++ convex/functions/demo.ts
@@ -1,7 +1,8 @@
+import { v } from "convex/values";
 import { query } from "../_generated/server";
 
 export const hello = query({
-  args: { name: "world" },
+  args: { name: v.string() },
   handler: async (ctx, args) => {
     return `Hello ${args.name}!`;
   },
