fab33074e3bd1f6bc1c99c5a3f9e332b1183797e
Jack Kunz <jacquesvfd@gmail.com>
2025-09-13 22:43:05 -0400

    Navbar wordmark: set icon vertical offset to 3px (-translate-y-[3px])

--- apps/teacher-web/app/_components/WordmarkLink.tsx
+++ apps/teacher-web/app/_components/WordmarkLink.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function WordmarkLink() {
   return (
     <Link href="/dashboard" className="flex items-center gap-0.5 hover:opacity-80 transition" aria-label="FlameLink home">
-      <Image src="/logo.svg" alt="" width={20} height={20} className="h-5 w-5 -translate-y-[1px]" />
+      <Image src="/logo.svg" alt="" width={20} height={20} className="h-5 w-5 -translate-y-[3px]" />
       <span className="text-lg font-semibold text-black">FlameLink</span>
       <span className="text-lg font-medium text-green-600">Instructor</span>
     </Link>
