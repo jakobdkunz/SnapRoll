749b084254c20df0b18ab79dccca46e21bc5f08f
Jakob Kunz <jakobdkunz@gmail.com>
2025-11-27 00:48:17 -0500

    Fix NativeWind babel config: spread plugins array instead of using object with .plugins property

--- apps/student-mobile/babel.config.js
+++ apps/student-mobile/babel.config.js
@@ -1,11 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+  const nativewindBabel = require('nativewind/babel');
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
+      ...nativewindBabel({}).plugins,
       // react-native-reanimated plugin must be listed last
       require.resolve('react-native-reanimated/plugin'),
     ],
