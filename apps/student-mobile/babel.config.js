ffe76a766062f5647c1328637b320c7bbf20c028
Jack Kunz <jacquesvfd@gmail.com>
2025-11-27 00:40:06 -0500

    Fix Babel config: Add NativeWind v4 plugin

--- apps/student-mobile/babel.config.js
+++ apps/student-mobile/babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      // react-native-reanimated plugin must be listed last
+      require.resolve('react-native-reanimated/plugin'),
+    ],
   };
 };
 
