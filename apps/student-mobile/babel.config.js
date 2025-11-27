80b3824ad6bdbf27791352c68389976a19d967b3
Jack Kunz <jacquesvfd@gmail.com>
2025-11-27 00:51:47 -0500

    Fix NativeWind babel: require and spread plugins instead of using as preset string

--- apps/student-mobile/babel.config.js
+++ apps/student-mobile/babel.config.js
@@ -1,10 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   const nativewindBabel = require('nativewind/babel');
+  const nativewindResult = nativewindBabel(api);
+  
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+    ],
     plugins: [
-      ...nativewindBabel({}).plugins,
+      ...(nativewindResult.plugins || []).filter(Boolean),
       // react-native-reanimated plugin must be listed last
       require.resolve('react-native-reanimated/plugin'),
     ],
